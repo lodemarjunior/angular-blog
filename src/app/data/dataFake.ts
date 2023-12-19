65b4ce02fee22b12a2d4b9980aa2aebe72f9c5ce
Lodemar Sansao Junior <lodemarjunior@github.com>
2023-12-18 22:21:03 -0300

    project end

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "Saiu a nova versão do Angular",
+        "description": "Versão do Angular 9.0 saiu...",
+        "photoCover": "https://cdn3.iconfinder.com/data/icons/logos-and-brands-adobe/512/21_Angular-1024.png"
+    },
+    {
+        "id": "2",
+        "title": "Estrutura do Angular",
+        "description": "Essa é a estrutura do Angular.",
+        "photoCover": "https://cdn.adioma.com/assets/post_infographics/depart/angular.png"
+    },
+    {
+        "id": "3",
+        "title": "Angular JS",
+        "description": "Angular JS é diferente de Angular 2!",
+        "photoCover": "https://th.bing.com/th/id/OIP.GQp0LFN9JU2MOyosALw4oQAAAA?rs=1&pid=ImgDetMain"
+    },
+    {
+        "id": "4",
+        "title": "Angular world",
+        "description": "Aprendendo Angular!",
+        "photoCover": "https://s3-us-west-2.amazonaws.com/angular-templates/imgs/angularmeta.png"
+    }
+]
